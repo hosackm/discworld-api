a1c6ce955a743e3ae667f36014701ccdc669d3c3
Matt Hosack <hosack.matt@gmail.com>
2020-07-27 12:58:59 -0700

    Adding tests for rate limiter.  Adding test database files to gitignore.

--- tests/test_limits.py
+++ tests/test_limits.py
@@ -1,5 +1,24 @@
-def test_endpoints_are_limited_correctly():
-    # for loop of 50 time per minute (or whatever value is)
-    # make sure the next one is rate limited
+import pytest
 
-    assert True
+
+@pytest.mark.parametrize("endpoint",
+                         [("get", "/api/books"),
+                          ("post", "/api/books"),
+                          ("patch", "/api/books/1"),
+                          ("delete", "/api/books/1"),
+                          ("get", "/api/subseries"),
+                          ("post", "/api/subseries"),
+                          ("patch", "/api/subseries/1"),
+                          ("delete", "/api/subseries/1"),
+                          ])
+def test_api_is_rate_limited(client, endpoint):
+    method, url = endpoint
+    method = getattr(client, method)
+
+    # exhaust available requests to activate rate limiter
+    for _ in range(50):
+        method(url)
+
+    # make sure the next request is rate limited
+    resp = method(url)
+    assert resp.status_code == 429
